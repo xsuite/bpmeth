da82b4cb1f44d9669530e537ccda06c2e296e06c
Silke Van der Schueren <silkevdschueren@gmail.com>
2024-03-28 17:46:40 +0100

    class for magnet fieldmaps

--- fieldmaps/hitriplus_cct/read.py
+++ fieldmaps/hitriplus_cct/read.py
@@ -3,33 +3,44 @@ import gzip
 import pyvista as pv
 
 
-# Data in global coordinate system
-data=np.loadtxt(gzip.open('fieldmap-cct.txt.gz'),skiprows=9)
-src=pv.PolyData(data.T[:3].T)
-src['Bx']=data.T[3].T
-src['By']=data.T[4].T
-src['Bz']=data.T[5].T
+class Magnet:
+    def __init__(self, data):
+        self.data = data
+        self.src = pv.PolyData(self.data.T[:3].T)
+        self.src['Bx'] = self.data.T[3].T
+        self.src['By'] = self.data.T[4].T
+        self.src['Bz'] = self.data.T[5].T
 
 
-# Curved coordinate system for the hitriplus cct magnet
-s,psi,r=np.meshgrid(np.linspace(-1,1,101),
-                    np.linspace(0,2*np.pi,33),
-                    np.linspace(0.0,0.1,5))
+    def plot(self):
+        self.src.plot()
+        
+
+    def curvedplot(self, rho, rmax, field, radius=0.1, n_psi=32):
+        s, psi, r = np.meshgrid(np.linspace(-1, 1, 101),
+                                np.linspace(0, 2*np.pi, n_psi+1),
+                                np.linspace(0.0, rmax, 5))
+
+        x = np.cos(s/rho) * (rho+r*np.cos(psi))
+        y = np.sin(s/rho) * (rho+r*np.cos(psi))
+        z = r*np.sin(psi)
+        xyz = np.array([x.flatten(), y.flatten(), z.flatten()]).T
+        dst = pv.PolyData(xyz)
+
+        dst=dst.interpolate(self.src, radius=radius)
+        dst.plot(scalars=field)
 
 
-R=1.65
 
-x=np.cos(s/R)*(R+ r*np.cos(psi))
-y=np.sin(s/R)*(R+ r*np.cos(psi))
-z=r*np.sin(psi)
-xyz=np.array([x.flatten(),y.flatten(),z.flatten()]).T
-dst=pv.PolyData(xyz)
 
-#pl=pv.Plotter()
-#pl.add_mesh(src)
-#pl.add_mesh(dst)
-#pl.show()
 
-# Interpolation of data on curved coordinate system
-dst=dst.interpolate(src, radius=0.1)
-dst.plot(scalars="Bz")
+data = np.loadtxt(gzip.open('fieldmap-cct.txt.gz'),skiprows=9)
+cctmagnet = Magnet(data)
+
+cctmagnet.plot()
+
+rho = 1.65
+rmax = 0.1
+cctmagnet.curvedplot(rho, rmax, "Bz")
+cctmagnet.curvedplot(rho, rmax, "Bz", n_psi=2)
+
