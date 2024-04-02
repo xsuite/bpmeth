15d8e2f56df03c20e0417f50775b066fcb117296
Silke Van der Schueren <silkevdschueren@gmail.com>
2024-04-02 18:12:27 +0200

    First version of fitting in s

--- fieldmaps/hitriplus_cct/read.py
+++ fieldmaps/hitriplus_cct/read.py
@@ -2,6 +2,7 @@ import numpy as np
 import gzip
 import pyvista as pv
 import matplotlib.pyplot as plt
+import scipy as sc
 
 
 class Magnet:
@@ -17,14 +18,14 @@ class Magnet:
         self.src.plot()
         
 
-    def curved_plot(self, rho, rmax, field, radius=0.1, n_psi=32):
+    def curved_plot(self, rho, rmax, smax, field, radius=0.01, n_psi=32, n_r=5, n_s=101):
         """
         Plot the field in a curved reference frame
         """ 
 
-        s, psi, r = np.meshgrid(np.linspace(-1, 1, 101),
+        s, psi, r = np.meshgrid(np.linspace(-smax, smax, n_s),
                                 np.linspace(0, 2*np.pi, n_psi+1),
-                                np.linspace(0.0, rmax, 5))
+                                np.linspace(0.0, rmax, n_r))
 
         X = np.cos(s/rho) * (rho+r*np.cos(psi))
         Y = np.sin(s/rho) * (rho+r*np.cos(psi))
@@ -38,7 +39,7 @@ class Magnet:
         return dst
 
 
-    def calc_x(self, spos, rho, rmax, field, radius=0.1, n_r=25, plot=False):
+    def calc_x(self, rho, rmax, spos, field, radius=0.01, n_r=25, plot=False):
         """
         Calculate profile of field in x direction at y=0 and given s
         """
@@ -63,16 +64,15 @@ class Magnet:
         return dst
 
 
-    def fit_x(self, spos, rho, rmax, field, radius=0.1, n_r=25, degree=5, plot=False):
+    def fit_x(self, rho, rmax, spos, field, radius=0.01, n_r=25, degree=10, plot=False):
         """
         Fit a polynomial to the x distribution for y=0 and a given s
         Bx(x, y=0, s) = a_j(s) x^j/j!
         By(x, y=0, s) = b_j(s) x^j/j!
         """
 
-        dst = self.calc_x(spos, rho, rmax, field, radius, n_r)
+        dst = self.calc_x(rho, rmax, spos, field, radius, n_r)
         coefs = np.polyfit(dst.point_data["xFS"], dst.point_data[field], degree)
-        print(coefs)
 
         if plot:
             x = np.linspace(min(dst.point_data["xFS"]), max(dst.point_data["xFS"]), 100)
@@ -84,15 +84,49 @@ class Magnet:
         return coefs
     
 
+    def fit_s(self, rho, rmax, smax, field, radius=0.01, n_r=25, n_s=25, xdegree=10, plot=False):
+        """
+        Fit a given function model(s, params) to the coefficients aj and bj
+        """
+
+        # Collect all aj(si)
+        s = np.linspace(-smax, smax, n_s)
+        coeflist = np.zeros((n_s, xdegree+1))
+        for i, spos in enumerate(s):
+            coefs = self.fit_x(rho, rmax, spos, field, radius, n_r, xdegree)
+            coeflist[i, :] = coefs
+        
+        # Fit a model function to every aj 
+        params = [0 for j in range(xdegree)]
+        for j in range(xdegree):
+            params[j] = sc.optimize.curve_fit(model, s, coeflist[:, j])
+
+        return params
+
+        
+def model(s, c0, c1, c2, c3, c4):
+    """
+    Model for aj(s), bj(s), all parameters have to be separate arguments.
+    """
+
+    return c0 + c1*s + c2*s**2 + c3*s**3 + c4*s**4
+
+
+
 
 
 data = np.loadtxt(gzip.open('fieldmap-cct.txt.gz'),skiprows=9)
 cctmagnet = Magnet(data)
 
-cctmagnet.plot()
+#cctmagnet.plot()
 
 rho = 1.65
 rmax = 0.1
-#cctmagnet.curved_plot(rho, rmax, "Bz")
-cctmagnet.curved_plot(rho, rmax, "Bz", n_psi=2)
-cctmagnet.fit_x(0.1, rho, rmax, "Bz", plot=True)
+smax = 0.8
+#cctmagnet.curved_plot(rho, rmax, smax, "Bz")
+#cctmagnet.curved_plot(rho, rmax, smax, "Bz", n_psi=2)
+
+spos=0.1
+#cctmagnet.fit_x(rho, rmax, spos, "Bz", plot=True)
+
+cctmagnet.fit_s(rho, rmax, smax, "Bz")
