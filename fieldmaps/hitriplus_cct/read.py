da76f594882db3f48ddbb743fc8c361c69042a58
Silke Van der Schueren <silkevdschueren@gmail.com>
2024-04-05 16:48:22 +0200

    Better plotting

--- fieldmaps/hitriplus_cct/read.py
+++ fieldmaps/hitriplus_cct/read.py
@@ -29,7 +29,7 @@ class Magnet:
                                 np.linspace(0.0, rmax, n_r))
 
         X = np.cos(s/rho) * (rho+r*np.cos(psi))
-        Y = np.sin(s/rho) * (rho+r*np.cos(psi))
+        Y = -np.sin(s/rho) * (rho+r*np.cos(psi))
         Z = r*np.sin(psi)
         XYZ = np.array([X.flatten(), Y.flatten(), Z.flatten()]).T
         dst = pv.PolyData(XYZ)
@@ -40,9 +40,11 @@ class Magnet:
         return dst
 
 
-    def calc_x(self, rho, rmax, spos, field, radius=0.01, n_r=25, plot=False):
+    def calc_x(self, rho, rmax, spos, radius=0.01, n_r=25, plot=False, fields=["Bx", "By", "Bz"]):
         """
         Calculate profile of field in x direction at y=0 and given s
+
+        :param fields: Fields that are desired to plot
         """
 
         s, psi, r = np.meshgrid([spos],
@@ -50,7 +52,7 @@ class Magnet:
                                 np.linspace(-rmax, rmax, n_r))
 
         X = np.cos(s/rho) * (rho+r*np.cos(psi))
-        Y = np.sin(s/rho) * (rho+r*np.cos(psi))
+        Y = -np.sin(s/rho) * (rho+r*np.cos(psi))
         Z = r*np.sin(psi)
         XYZ = np.array([X.flatten(), Y.flatten(), Z.flatten()]).T
         dst = pv.PolyData(XYZ)
@@ -59,24 +61,35 @@ class Magnet:
 
         # Frenet-Serret coordinates
         dst["xFS"] = np.array(r*np.cos(psi)).flatten()
-        dst["yFS"] = 0 * s.flatten()
+        dst["yFS"] = np.array(r*np.sin(psi)).flatten()
         dst["sFS"] = s.flatten()
 
         # Field in Frenet-Serret coordinates
-        dst["BxFS"] = dst["Bx"] * np.cos(s/rho).flatten() + dst["By"] * np.sin(s/rho).flatten()
+        dst["BxFS"] = dst["Bx"] * np.cos(s/rho).flatten() - dst["By"] * np.sin(s/rho).flatten()
         dst["ByFS"] = dst["Bz"]
-        dst["BsFS"] = dst["Bx"] * np.sin(s/rho).flatten() - dst["By"] * np.cos(s/rho).flatten()
+        dst["BsFS"] = dst["Bx"] * np.sin(s/rho).flatten() + dst["By"] * np.cos(s/rho).flatten()
        
         if plot:
-            plt.scatter(dst.point_data["xFS"], dst.point_data[field])
+            fig, ax = plt.subplots()
+            if "Bx" in fields:
+                ax.scatter(dst.point_data["xFS"], dst.point_data["BxFS"], color="orange", s=3, label=f"$B_x(x, y=0, s={spos})$")
+            if "By" in fields:
+                ax.scatter(dst.point_data["xFS"], dst.point_data["ByFS"], color="blue", s=3, label=f"$B_y(x, y=0, s={spos})$")
+            if "Bz" in fields:
+                ax.scatter(dst.point_data["xFS"], dst.point_data["BsFS"], color="lightgreen", s=3, label=f"$B_s(x, y=0, s={spos})$")
+            ax.set_xlabel("x (m)")
+            ax.set_ylabel("Field (T)")
+            ax.legend(loc="upper right")
             plt.show()
 
         return dst
 
 
-    def calc_s(self, rho, smax, field, radius=0.01, n_s=101, plot=False):
+    def calc_s(self, rho, smax, radius=0.01, n_s=101, plot=False, fields=["Bx", "By", "Bs"]):
         """
         Calculate profile of field in s direction at x=y=0
+
+        :param fields: Fields that are desired to plot
         """
 
         s, psi, r = np.meshgrid(np.linspace(-smax, smax, n_s),
@@ -84,7 +97,7 @@ class Magnet:
                                 [0.0])
 
         X = np.cos(s/rho) * (rho+r*np.cos(psi))
-        Y = np.sin(s/rho) * (rho+r*np.cos(psi))
+        Y = -np.sin(s/rho) * (rho+r*np.cos(psi))
         Z = r*np.sin(psi)
         XYZ = np.array([X.flatten(), Y.flatten(), Z.flatten()]).T
         dst = pv.PolyData(XYZ)
@@ -92,19 +105,26 @@ class Magnet:
         dst = dst.interpolate(self.src, radius=radius)
 
         # Frenet-Serret coordinates
-        dst["xFS"] = 0 * s.flatten()
-        dst["yFS"] = 0 * s.flatten()
+        dst["xFS"] = np.array(r*np.cos(psi)).flatten()
+        dst["yFS"] = np.array(r*np.sin(psi)).flatten()
         dst["sFS"] = s.flatten()
 
         # Field in Frenet-Serret coordinates
-        dst["BxFS"] = dst["Bx"] * np.cos(s/rho).flatten() + dst["By"] * np.sin(s/rho).flatten()
+        dst["BxFS"] = dst["Bx"] * np.cos(s/rho).flatten() - dst["By"] * np.sin(s/rho).flatten()
         dst["ByFS"] = dst["Bz"]
-        dst["BsFS"] = dst["Bx"] * np.sin(s/rho).flatten() - dst["By"] * np.cos(s/rho).flatten()
+        dst["BsFS"] = dst["Bx"] * np.sin(s/rho).flatten() + dst["By"] * np.cos(s/rho).flatten()
        
         if plot:
             fig, ax = plt.subplots()
-            ax.scatter(dst.point_data["sFS"], dst.point_data[field], label=field)
-            ax.legend()
+            if "Bx" in fields:
+                ax.scatter(dst.point_data["sFS"], dst.point_data["BxFS"], label="$B_x(x=y=0, s)$", s=3, color="orange")
+            if "By" in fields:
+                ax.scatter(dst.point_data["sFS"], dst.point_data["ByFS"], label="$B_y(x=y=0, s)$", s=3, color="blue")
+            if "Bs" in fields:
+                ax.scatter(dst.point_data["sFS"], dst.point_data["BsFS"], label="$B_s(x=y=0, s)$", s=3, color="lightgreen")
+            ax.set_xlabel("s (m)")
+            ax.set_ylabel("Field (T)")
+            ax.legend(loc="upper right")
             plt.show()
 
         return dst
@@ -122,11 +142,17 @@ class Magnet:
         # In order to calculate the derivative in zero we want to have interpolation at zero, 
         # so we need n_r to be odd for the interpolation
         n_r = n_r + 1 - n_r%2
-        dst = self.calc_x(rho, rmax, spos, field, radius, n_r)
+        dst = self.calc_x(rho, rmax, spos, radius, n_r)
 
         # Extract the multipole coefficients by fitting polynomial
         params = np.polyfit(dst.point_data["xFS"], dst.point_data[field], degree)
         coefsfit = [params[degree-i] * math.factorial(i) for i in range(degree+1)]
+
+        # Extract the multipole coefficients by fitting polynomial only to the central region
+        fitmin = 3*n_r//10
+        fitmax = n_r - fitmin
+        params = np.polyfit(dst.point_data["xFS"][fitmin:fitmax], dst.point_data[field][fitmin:fitmax], degree)
+        coefsfitcenter = [params[degree-i] * math.factorial(i) for i in range(degree+1)]
 
         # Extract the multipole coefficients by calculating derivatives, using central difference
         h = 4*rmax / (n_r-1)  # Twice the interval between two points, since we need half step values
@@ -138,34 +164,36 @@ class Magnet:
                 coefsdiff[n] += 1/h**n * (-1)**i * math.factorial(n)/math.factorial(n-i)/math.factorial(i) * dst.point_data[field][int(ind)]
 
         if plot:
-            x = np.linspace(min(dst.point_data["xFS"]), max(dst.point_data["xFS"]), 100)
+            x = np.linspace(min(dst.point_data["xFS"]), max(dst.point_data["xFS"]), 1000)
             yfit = sum([coefsfit[i] * x**(i) / math.factorial(i) for i in range(degree+1)])
+            yfitcenter = sum([coefsfitcenter[i] * x**(i) / math.factorial(i) for i in range(degree+1)])
             ydiff = sum([coefsdiff[i] * x**(i) / math.factorial(i) for i in range(degree+1)])
+
             fig, ax = plt.subplots()
-            ax.scatter(dst.point_data["xFS"], dst.point_data[field], label="Data")
-            ax.plot(x, yfit, label="Fit") 
-            ax.plot(x, ydiff, label="Derivatives") 
+            ax.scatter(dst.point_data["xFS"], dst.point_data[field], label=f"$B_x(x, y=0, s={spos})$", s=3, color="orange")
+            ax.plot(x, yfit, label="Fit", color="red") 
+            ax.plot(x, yfitcenter, label="Central fit", color="purple") 
+            ax.plot(x, ydiff, label="Derivatives", color="pink") 
+            ax.axvline(x=dst.point_data["xFS"][fitmin], color="gray")
+            ax.axvline(x=dst.point_data["xFS"][fitmax], color="gray")
             ax.set_ylim(1.1*min(dst.point_data[field]), 1.1*max(dst.point_data[field]))
             fig.legend()
             plt.show()
 
-        return coefsfit, coefsdiff
-
     
-    def magnetic_length(self, rho, smax, radius=0.01, n_s=101, plot=False):
+    def magnetic_length(self, rho, smax, radius=0.01, n_s=101, plot=False, fields=["Bx", "By", "Bs"]):
         """
         Calculate the magnetic length of the magnet
+
+        :param fields: Fields that are desired to plot
         """
         
         n_s = n_s + 1 - n_s%2
-        dst = self.calc_s(rho, smax, "ByFS", radius, n_s)
+        dst = self.calc_s(rho, smax, radius, n_s)
 
         Bint = sc.integrate.simpson(dst.point_data["ByFS"], x=dst.point_data["sFS"])
         [Bcenter] = dst.point_data["ByFS"][dst.point_data["sFS"]==0]
         mlen = Bint / Bcenter
-
-        print("Integrated field: ", Bint)
-        print("Central field: ", Bcenter)
 
         if plot:
             fig, ax = plt.subplots()
@@ -173,9 +201,12 @@ class Magnet:
             ax.axvline(x=-mlen/2, color="red")
             plt.text(mlen/2, 4.3, "$L_{mag}/2$", color="red", horizontalalignment="center")
             plt.text(-mlen/2, 4.3, "$L_{mag}/2$", color="red", horizontalalignment="center")
-            #ax.scatter(dst.point_data["sFS"], dst.point_data["BxFS"], label="$B_x(x=y=0, s)$", s=3, color="orange")
-            ax.scatter(dst.point_data["sFS"], dst.point_data["ByFS"], label="$B_y(x=y=0, s)$", s=3, color="blue")
-            ax.scatter(dst.point_data["sFS"], dst.point_data["BsFS"], label="$B_s(x=y=0, s)$", s=3, color="lightgreen")
+            if "Bx" in fields:
+                ax.scatter(dst.point_data["sFS"], dst.point_data["BxFS"], label="$B_x(x=y=0, s)$", s=3, color="orange")
+            if "By" in fields:
+                ax.scatter(dst.point_data["sFS"], dst.point_data["ByFS"], label="$B_y(x=y=0, s)$", s=3, color="blue")
+            if "Bs" in fields:
+                ax.scatter(dst.point_data["sFS"], dst.point_data["BsFS"], label="$B_s(x=y=0, s)$", s=3, color="lightgreen")
             ax.set_xlabel("s (m)")
             ax.set_ylabel("Field (T)")
             ax.legend(loc="upper right")
@@ -188,39 +219,35 @@ class Magnet:
 data = np.loadtxt(gzip.open('fieldmap-cct.txt.gz'),skiprows=9)
 cctmagnet = Magnet(data)
 
-#cctmagnet.plot()
+cctmagnet.plot()
 
 field = "Bz"
 rho = 1.65
 rmax = 0.1
 smax = 0.8
 
-#cctmagnet.curved_plot(rho, rmax, smax, field)
-#cctmagnet.curved_plot(rho, rmax, smax, field, n_psi=2)
-#
-#cctmagnet.curved_plot(rho, rmax, smax, "Bx", n_psi=2)
-#cctmagnet.curved_plot(rho, rmax, smax, "By", n_psi=2)
+cctmagnet.curved_plot(rho, rmax, smax, field)
+cctmagnet.curved_plot(rho, rmax, smax, field, n_psi=2)
+
+cctmagnet.curved_plot(rho, rmax, smax, "Bx", n_psi=2)
+cctmagnet.curved_plot(rho, rmax, smax, "By", n_psi=2)
+
+spos=0
+cctmagnet.calc_x(rho, rmax, spos, n_r=200, plot=True, fields="Bx")
+cctmagnet.calc_x(rho, rmax, spos, n_r=200, plot=True, fields="By")
 #
 spos=0.1
-#cctmagnet.calc_x(rho, rmax, spos, "Bx", plot=True)
-#cctmagnet.calc_x(rho, rmax, spos, "By", plot=True)
-#cctmagnet.calc_x(rho, rmax, spos, "Bz", plot=True)
-#
-#cctmagnet.calc_x(rho, rmax, spos, "BxFS", plot=True)
-#cctmagnet.calc_x(rho, rmax, spos, "ByFS", plot=True)
-#cctmagnet.calc_x(rho, rmax, spos, "BsFS", plot=True)
-#
+cctmagnet.calc_x(rho, rmax, spos, n_r=200, plot=True, fields="Bx")
+cctmagnet.calc_x(rho, rmax, spos, n_r=200, plot=True, fields="By")
+cctmagnet.fit_x(rho, rmax, spos, "ByFS", plot=True)
+
+cctmagnet.calc_s(rho, 1, plot=True)
+
 spos=0.1
-#cctmagnet.fit_x(rho, rmax, spos, "ByFS", plot=True)
+cctmagnet.fit_x(rho, rmax, spos, "BxFS", n_r = 55, degree = 20, plot=True)
+cctmagnet.fit_x(rho, rmax, spos, "ByFS", n_r = 55, degree = 20, plot=True)
 
-#cctmagnet.calc_s(rho, 1, "BxFS", plot=True)
-#cctmagnet.calc_s(rho, 1, "ByFS", plot=True)
-#cctmagnet.calc_s(rho, 1, "BsFS", plot=True)
-
-#cctmagnet.fit_x(rho, rmax, spos, "BxFS", n_r = 55, degree = 20, plot=True)
-#cctmagnet.fit_x(rho, rmax, spos, "ByFS", n_r = 55, degree = 20, plot=True)
-
-cctmagnet.magnetic_length(rho, smax, n_s=200, plot=True)
+cctmagnet.magnetic_length(rho, smax, n_s=200, plot=True, fields=["By", "Bs"])
 
 
 
