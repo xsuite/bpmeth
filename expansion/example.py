d6fecb39240567a7b7b531b1bfbba55125eaa094
Silke <silkevdschueren@gmail.com>
2025-01-15 15:58:42 +0100

    class to generate field expansion

---
+++ expansion/example.py
@@ -0,0 +1,14 @@
+import generate_expansion
+import numpy as np
+import sympy as sp
+
+fringe = generate_expansion.FieldExpansion(b=("(tanh(s)+1)/2",))
+fringe.plotfield()
+
+#xmin, xmax, xstep = -2, 2, 0.2
+#ymin, ymax, ystep = -2, 2, 0.2
+#zmin, zmax, zstep = -3, 3, 0.2
+#X = np.arange(xmin, xmax, xstep)
+#Y = np.arange(ymin, ymax, ystep)
+#Z = np.arange(zmin, zmax, zstep)
+#fringe.writefile(X, Y, Z, "tanh")
