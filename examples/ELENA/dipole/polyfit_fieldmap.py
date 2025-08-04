2eb96849b26cc854104b2c33ffa38041310aed6f
Silke <silkevdschueren@gmail.com>
2025-08-04 17:29:50 +0200

    Splines fieldmap without closed orbit effect

--- examples/ELENA/dipole/polyfit_fieldmap.py
+++ examples/ELENA/dipole/polyfit_fieldmap.py
@@ -15,24 +15,28 @@ theta_E = 17/180*np.pi
 B = 5.3810e-07
 Brho = B*rho
 
+kf_enge=1.0008781
 guess = [ 4.27855927e-01, 7.63365939e+02, -1.44485719e+02, 2.59387643e+01, 5.81379154e-01]
-dipole_enge = bpmeth.DipoleFromFieldmap(data, 1/rho, l_magn, design_field=1/rho, shape="enge", hgap=apt/2, apt=apt, radius=0.05, order=1, nphi=2, plot=True, guess=guess)
+dipole_enge = bpmeth.DipoleFromFieldmap(data, 1/rho, l_magn, design_field=1/rho*kf_enge, shape="enge", hgap=apt/2, apt=apt, radius=0.05, order=1, nphi=2, plot=False, guess=guess)
 
-dipole_splines = bpmeth.MagnetFromFieldmap(data, 1/rho, l_magn, design_field=1/rho, order=3, hgap=apt/2, nphi=2, plot=True, step=25)
+kf_splines=1.000979
+dipole_splines = bpmeth.MagnetFromFieldmap(data, 1/rho, l_magn, design_field=1/rho*kf_splines, order=3, hgap=apt/2, nphi=2, plot=True, symmetric=True, step=50)
 
 # For checking
 # dipole_splines_from_enge = bpmeth.MagnetFromFieldmap(data_enge, 1/rho, l_magn, design_field=1/rho, order=1, hgap=apt/2, nphi=2, plot=True, step=50, in_FS_coord=True)
 
 
 p = xt.Particles(y=np.linspace(-2e-3, 2e-3, 11), p0c=0.1, mass0=0.938272, q0=1)
-p_enge = p.copy()
 p_splines = p.copy()
 # p_splines_enge = p.copy()
 
-dipole_enge.track(p_enge, plot=True)
 dipole_splines.track(p_splines, plot=True)
 # dipole_splines_from_enge.track(p_splines_enge, plot=True)
 
-print(p_enge.s[0], p_enge.x[0], p_enge.y[0], p_enge.px[0], p_enge.py[0])
-print(p_splines.s[0], p_splines.x[0], p_splines.y[0], p_splines.px[0], p_splines.py[0])
-# print(p_splines_enge.s[0], p_splines_enge.x[0], p_splines_enge.y[0], p_splines_enge.px[0], p_splines_enge.py[0])
+print(p_splines.s[5], p_splines.x[5], p_splines.px[5], p_splines.y[5], p_splines.py[5])
+assert np.isclose(p_splines.s[5], dipole_splines.length)
+
+
+p_enge = p.copy()
+dipole_enge.track(p_enge, plot=True)
+print(p_enge.s[5], p_enge.x[5], p_enge.px[5], p_enge.y[5], p_enge.py[5])
