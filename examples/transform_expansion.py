3c8f4e18cf53ca40faf605b7eedfb1ee9d8df4ae
Silke <silkevdschueren@gmail.com>
2026-03-24 15:12:59 +0100

    Example for transformations on the field expansion

--- examples/transform_expansion.py
+++ examples/transform_expansion.py
@@ -4,10 +4,9 @@ import bpmeth
 import matplotlib.pyplot as plt
 import xtrack as xt
 
-
-"""
-
-"""
+#########################################
+# Transformation functions on expansion #
+#########################################
 
 angle = 10  # degrees
 angle_rad = angle/180*np.pi
@@ -16,11 +15,11 @@ length = 5
 A_fringe = bpmeth.GeneralVectorPotential(b=("0.1*(tanh(5*s)+1)/2",))
 A_rotated_fringe = A_fringe.transform(theta_E = angle_rad)
 
-A_fringe.plotfield_xz(Y=0.1, includeby=True, xmin=-0.5, xmax=0.5, xstep=0.01, zmin=-length/4, zmax=length/4, zstep=0.05)
-A_rotated_fringe.plotfield_xz(Y=0.1, includeby=True, xmin=-0.5, xmax=0.5, xstep=0.01, zmin=-length/4, zmax=length/4, zstep=0.05)
+A_fringe.plot_BxBs(Y=0.1, xmin=-0.5, xmax=0.5, xstep=0.01, smin=-length/4, smax=length/4, sstep=0.05)
+A_rotated_fringe.plot_BxBs(Y=0.1, xmin=-0.5, xmax=0.5, xstep=0.01, smin=-length/4, smax=length/4, sstep=0.05)
 
-H_fringe = bpmeth.Hamiltonian(length=length, curv=0, vectp=A_fringe)
-H_rotated_fringe = bpmeth.Hamiltonian(length=length, curv=0, vectp=A_rotated_fringe)
+H_fringe = bpmeth.Hamiltonian(length=length, h=0, vectp=A_fringe)
+H_rotated_fringe = bpmeth.Hamiltonian(length=length, h=0, vectp=A_rotated_fringe)
 
 p0 = xt.Particles(y=np.linspace(-1e-3, 1e-3, 10), s=-length/2, energy0=10e9, mass0=xt.ELECTRON_MASS_EV)
 p0_rotated = p0.copy()
