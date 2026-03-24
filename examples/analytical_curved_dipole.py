2b668136afb386877338ffe610c12c8e61111384
Silke <silkevdschueren@gmail.com>
2026-03-24 16:08:13 +0100

    Name change, updated example to plot trajectory in dipole

--- examples/analytical_curved_dipole.py
+++ examples/analytical_curved_dipole.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 length = 1
 angle = 1
 
-BB = 1 # Only q By / P0 plays a role so lets call this quantity BB
+b1 = 1
 x0 = 0
 y0 = 0
 px0 = 0
@@ -30,10 +30,10 @@ delta = np.sqrt(1 + 2*ptau0/beta0 + ptau0**2) - 1
 theta0 = np.arcsin(px0 / np.sqrt((1+delta)**2 - py0**2))
 
 s = np.linspace(0,1,100)
-px = px0*np.cos(s*h) + ( np.sqrt((1+delta)**2-px0**2-py0**2) - (1/h + x0)*BB )*np.sin(s*h)
-pxder = -px0*h*np.sin(s*h) + ( np.sqrt((1+delta)**2-px0**2-py0**2) - (1/h + x0)*BB )*h*np.cos(s*h)
-x = 1/h * 1/BB * ( h*np.sqrt((1+delta)**2-px**2-py0**2) - pxder - BB)
-y = h*py0/BB*s - py0/BB * ( np.arcsin(px/np.sqrt((1+delta)**2)) - np.arcsin(px0/np.sqrt((1+delta)**2)) ) + y0
+px = px0*np.cos(s*h) + ( np.sqrt((1+delta)**2-px0**2-py0**2) - (1/h + x0)*b1 )*np.sin(s*h)
+pxder = -px0*h*np.sin(s*h) + ( np.sqrt((1+delta)**2-px0**2-py0**2) - (1/h + x0)*b1 )*h*np.cos(s*h)
+x = 1/h * 1/b1 * ( h*np.sqrt((1+delta)**2-px**2-py0**2) - pxder - b1)
+y = h*py0/b1*s - py0/b1 * ( np.arcsin(px/np.sqrt((1+delta)**2)) - np.arcsin(px0/np.sqrt((1+delta)**2)) ) + y0
 
 fb.plot_trajectory_zx(s,x,y)
 fb.plot_trajectory_zxy(s,x,y)
