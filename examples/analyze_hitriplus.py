91c6f0c6e43cd992b3455f3da5a41b08b2048870
Silke <silkevdschueren@gmail.com>
2026-04-09 11:12:14 +0200

    Example on hitriplus fieldmap

--- examples/analyze_hitriplus.py
+++ examples/analyze_hitriplus.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import gzip
 import bpmeth
+import math
 
 
 rho = 1.65
@@ -9,7 +10,7 @@ phi = 30/180*np.pi
 l_magn = phi*rho
 apt = 0.08  # aperture of the magnet
 
-data = np.loadtxt(gzip.open('fieldmap-cct.txt.gz'), usecols=[0,2,1,3,5,4], skiprows=9)
+data = np.loadtxt(gzip.open('../fieldmaps/hitriplus_cct/fieldmap-cct.txt.gz'), usecols=[0,2,1,3,5,4], skiprows=9)
 cctmagnet = bpmeth.Fieldmap(data)
 # cctmagnet.FS_frame_plot(rho, phi, "By", xmax=apt, ymax=0, nx=51, ny=1, ns=201, smax=0.9, radius=0.005)
 
@@ -18,8 +19,8 @@ yFS = [0]
 sFS = np.linspace(-0.9*l_magn, 0.9*l_magn, 501)
 cctmagnet_FS = cctmagnet.calc_FS_coords(xFS, yFS, sFS, rho, phi, radius=0.005)
 
-method="polynomial"
-# method="finite_difference"
+#method="polynomial"
+method="finite_difference"
 fig, ax = plt.subplots(figsize=(6,4))
 cctmagnet_FS.s_multipoles(3, ax=ax, xmax=apt/2, method=method)
 ax.set_yscale('symlog')
